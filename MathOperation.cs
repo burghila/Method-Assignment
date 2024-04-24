ce25d5b954dc057244af648f91f6d956fd0bed88
Alin Burghila <108270121+burghila@users.noreply.github.com>
2024-04-24 20:02:03 +0100

    Add files via upload

---
+++ MathOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class MathOperation1
+    {
+        // Creates a method that takes two integers as parameters
+        // The second parameter is optional and has a default value of 1
+        public int MathOperation(int i, int j = 1)
+        {
+            // Performs multiplication on integers and return the result
+            int result = i * j;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate MathOperation1 class
+            MathOperation1 mathOp = new MathOperation1();
+
+            // Ask the user to provide the first parameter
+            Console.WriteLine("Enter an integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to provide a second parameter or press enter
+            Console.WriteLine("Enter a second integer, or just press enter: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                // If a second number is provided, parse it and call the method with both numbers
+                int num2 = Convert.ToInt32(input);
+                int results = mathOp.MathOperation(num1, num2);
+                Console.WriteLine($"{num1} x {num2} = {results}");
+            }
+            catch
+            {
+                // If a second number is not provided, call the method with the first number only
+                int results = mathOp.MathOperation(num1);
+                Console.WriteLine($"{num1} x Default 1 = {results}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
